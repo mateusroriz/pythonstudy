fec07b6bf5f5be765ee215d8b11c62b4979fe303
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-04-30 01:55:37 -0300

    adding basic features like repetition

--- jogo.py
+++ jogo.py
@@ -1,14 +1,27 @@
 print("Jogo de advinhação!")
 
 numero_secreto = 42
+total_de_tentativas = 5
 
-chute_str = input("digite o seu numero: ")
+while(total_de_tentativas > 0):
+    #print("Você tem mais: {} ", total_de_tentativas, "tentativas")
+    print("Você tem mais: {}  tentativas".format (total_de_tentativas))
+    chute = int(input("digite o seu numero: ")) #converting the str to int right in the input
 
-print("Você digitou ", chute_str)
+    if(chute):
+        total_de_tentativas -=1
 
-chute = int(chute_str) #int() convert the str to int so it returns no errors
+    print("Você digitou ", chute)
 
-if(numero_secreto == chute):
-    print("acertou")
-else:
-    print('ERROU!')
+    acertou = chute == numero_secreto
+    maior = chute> numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+        exit()
+    else:
+        if(maior):
+                print("Você errou! O seu chute foi maior que o número secreto")
+        elif(menor):
+                print("Você errou! O seu chute foi menor que o número secreto")
