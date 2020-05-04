fec59f9931b385c2b4a2d43390fda0fa76fcaa70
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-04 01:39:04 -0300

    addding barebones to hangman game

--- forca.py
+++ forca.py
@@ -1,5 +1,23 @@
 def jogar():
     print("Bem vindo ao jogo da forca")
+
+    palavra_secreta = "Python"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?").strip() #pegando letra do usuário e retirando espaços com strip()
+
+
+        index = 0
+        for letra in palavra_secreta: #iterando cada caractere da palavra secreta
+            if(chute.lower() == letra.lower()):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index +=1 #aumentando o index a cada loop
+
+        print("jogando...")
+
     print("TODO: Fim do jogo")
 
 if(__name__ == "__main__"): #se esse programa for o principal rodar isso
