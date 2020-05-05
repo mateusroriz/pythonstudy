6726b659d9625f881c874414f15ecbeb21decd3a
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-05 03:21:15 -0300

    adding a list to store found letters in game

--- forca.py
+++ forca.py
@@ -2,21 +2,24 @@ def jogar():
     print("Bem vindo ao jogo da forca")
 
     palavra_secreta = "Python"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra?").strip() #pegando letra do usuário e retirando espaços com strip()
 
-
         index = 0
         for letra in palavra_secreta: #iterando cada caractere da palavra secreta
             if(chute.lower() == letra.lower()):
-                print("Encontrei a letra {} na posição {}".format(letra,index))
+                letras_acertadas[index] = letra # se acertar colocar a letra acertada na lista letras_acertadas
             index +=1 #aumentando o index a cada loop
 
-        print("jogando...")
+        print(letras_acertadas)
 
     print("TODO: Fim do jogo")
 
