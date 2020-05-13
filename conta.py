ff981043c38fa1b6aa7cd7e42d2504baa91668c0
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-13 02:57:19 -0300

    using statics methods to define class functions without the need to create an object

--- conta.py
+++ conta.py
@@ -1,6 +1,6 @@
 
 class Conta:
-
+    #construtor
     def __init__(self, numero, titular, saldo, limite): #self é a referencia para encontrar o objeto quando criado
         print("Construindo objeto ... {}".format(self))
         self.__numero = numero
@@ -14,13 +14,20 @@ class Conta:
     def depositar(self,valor):
         self.__saldo += valor
 
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar #return true or false
+
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transferir(self, valor , destino):
         self.sacar(valor)
         destino.depositar(valor)
-        
+
     @property
     def saldo(self):
         return self.__saldo
@@ -36,3 +43,11 @@ class Conta:
     @limite.setter #usando property em um getter pra modificar o valor do limite
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco(): #metodo estatico é da classe, nao precisa do objeto
+        return "001"
+
+    @staticmethod
+    def codigos_bancos(): #metodo estatico é da classe, nao precisa do objeto
+        return {"BB" : "001", "Caixa" : "104", "Bradesco": "237"}
