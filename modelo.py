5c7b29152258a564abc3c5a27fc09df79906d680
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-15 22:01:47 -0300

    using polimorfism to print expecific informations in each object

--- modelo.py
+++ modelo.py
@@ -19,17 +19,25 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self): #representando objeto como texto
+        return (f'{self._nome} - {self.ano} - {self._likes} Likes')
 
 class Filme(Programa): #herdando da classe Programa
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano) #super chama a classe mae nesse caso o inicializador dela
         self.duracao = duracao
 
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.duracao} Min - {self._likes} Likes')
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome,ano) #super chama qualquer metodo da classe mae
         self.temporadas = temporadas
+
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes')
 
 
 
@@ -42,5 +50,7 @@ vingadores.dar_like()
 westworld.dar_like()
 westworld.dar_like()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {westworld.nome} - Likes: {westworld.likes}')
+filmes_e_series = [vingadores, westworld]
+
+for programa in filmes_e_series:
+    print(programa)
