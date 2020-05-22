dbb6004d2c6e8491b9fd555bb2dedafceb47cd9b
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-21 22:00:46 -0300

    adding exceptions treatment to prevent concorrency errors

--- principal.py
+++ principal.py
@@ -1,5 +1,10 @@
-arquivo_contatos = open('contatos.csv', encoding='latin_1')
+try:
+    with open('contatos.csv', encoding='UTF-8') as arquivo_contatos: #opening the file just when we are using it then closing it
+        for linha in arquivo_contatos:
+            print(linha, end='')
 
+except FileNotFoundError:
+    print("Arquivo não encontrado")
 
-for linha in arquivo_contatos:
-    print(linha, end='')
+except PermissionError:
+    print("Sem permissão de escrita")
