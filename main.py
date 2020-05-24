6427173c9d990625db7be450fe919d68ffd569cd
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-24 18:23:29 -0300

    adding new project to test string manipulation

---
+++ main.py
@@ -0,0 +1,6 @@
+#argumento = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
+
+argumento = "moedaorigem=real"
+
+listaargumento = argumento.split("=")
+print(listaargumento)
