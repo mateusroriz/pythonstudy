1bfabf1b6da63873d47f12654dff7bac37ab35c4
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-30 20:53:52 -0300

    Creating list to hold itens

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__) #__name__ it's the package name
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route("/inicio") #defining a route for the function
 def ola():
-    return render_template("lista.html")
+    jogo1 = Jogo("Super Mario", "Plataforma", "Snes")
+    jogo2 = Jogo("Pkm red", "Rpg", "Gameboy")
+    jogo3 = Jogo("MK", "Luta", "SNES")
+    lista = [jogo1, jogo2, jogo3]
+    return render_template("lista.html", titulo ="Jogos", jogos = lista)
 
 app.run()
