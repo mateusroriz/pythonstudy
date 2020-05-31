dc5cf3ac06aa81fc8fa078d27650139c50865b3f
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-05-31 19:11:56 -0300

    adding simple form to add information to webapp

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__) #__name__ it's the package name
 
@@ -8,12 +8,28 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route("/inicio") #defining a route for the function
-def ola():
-    jogo1 = Jogo("Super Mario", "Plataforma", "Snes")
-    jogo2 = Jogo("Pkm red", "Rpg", "Gameboy")
-    jogo3 = Jogo("MK", "Luta", "SNES")
-    lista = [jogo1, jogo2, jogo3]
+@app.route("/") #defining a route for the function
+def index():
     return render_template("lista.html", titulo ="Jogos", jogos = lista)
 
-app.run()
+
+jogo1 = Jogo("Super Mario", "Plataforma", "Snes")
+jogo2 = Jogo("Pkm red", "Rpg", "Gameboy")
+jogo3 = Jogo("MK", "Luta", "SNES")
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/novo')
+def novo():
+    return render_template("novo.html", titulo = "Novo Jogo")
+
+@app.route("/criar", methods =["POST", ] )
+def criar():
+    nome = request.form["nome"]
+    categoria = request.form["categoria"]
+    console = request.form["console"]
+    jogo = Jogo(nome,categoria,console)
+    lista.append(jogo)
+    return render_template("lista.html", titulo= "Jogo", jogos = lista)
+
+
+app.run(debug = True)
