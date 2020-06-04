5edff940cd7d3583f72dc5ade854e297f4a23d4e
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-06-03 22:14:56 -0300

    adding simple authorization and autenthication for multiple users

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__) #__name__ it's the package name
 app.secret_key = "cryptasecret"
@@ -9,18 +9,31 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route("/") #defining a route for the function
-def index():
-    return render_template("lista.html", titulo ="Jogos", jogos = lista)
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
 
+usuario1 = Usuario('ciclano', "Ciclano", '1234')
+usuario2 = Usuario("fulano", "Fulano Ciclano", "js")
+usuario3 = Usuario("deltrano", "Deltrano", "python")
+
+usuarios = {usuario1.id: usuario1, usuario2.id: usuario2, usuario3.id: usuario3}
 
 jogo1 = Jogo("Super Mario", "Plataforma", "Snes")
 jogo2 = Jogo("Pkm red", "Rpg", "Gameboy")
 jogo3 = Jogo("MK", "Luta", "SNES")
 lista = [jogo1, jogo2, jogo3]
 
+@app.route("/") #defining a route for the function
+def index():
+    return render_template("lista.html", titulo ="Jogos", jogos = lista)
+
 @app.route('/novo')
 def novo():
+    if "usuario_logado" not in session or session["usuario_logado"] == None:
+        return redirect(url_for("login", proxima=url_for("novo"))) #novo é o caminho a seguir depois de dar login
     return render_template("novo.html", titulo = "Novo Jogo")
 
 @app.route("/criar", methods =["POST", ] )
@@ -30,28 +43,32 @@ def criar():
     console = request.form["console"]
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for("index"))
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    proxima = request.args.get("proxima")
+    return render_template("login.html", proxima = proxima ) #jogando informação pro formulario
 
 
 @app.route("/autenticar", methods=["POST"])
-def autentiar():
-    if "mestra" == request.form['senha']:
-        session['usuario_logado'] = request.form["usuario"]
-        flash(request.form["usuario"] + " logou com sucesso")
-        return redirect("/")
+def autenticar():
+    if request.form["usuario"] in usuarios:
+        usuario = usuarios[request.form["usuario"]]
+        if usuario.senha == request.form["senha"]:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + " logou com sucesso")
+            proxima_pagina = request.form["proxima"]
+            return redirect(proxima_pagina)
     else:
         flash("Não logado, tente novamente")
-        return redirect("/login")
+        return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
     session["usuario_logado"] = None
     flash("Nenhum usuário logado")
-    return redirect("/")
+    return redirect(url_for("index"))
 
 app.run(debug = True)
